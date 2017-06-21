4e91cf0840634315db12acdfe6f1fffe49d2faea
Kirill Sevastyanenko <kirillseva@gmail.com>
2017-06-21 16:33:40 -0500

    too verbose

--- restore.sh
+++ restore.sh
@@ -4,7 +4,7 @@ LOCKBOX_TMP=/tmp/lockboxcache.tar.gz
 
 aws s3 cp $LOCKBOX_S3_PATH $LOCKBOX_TMP
 mkdir -p $LOCKBOX_DIRECTORY
-tar -C $LOCKBOX_DIRECTORY -xzvf $LOCKBOX_TMP
+tar -C $LOCKBOX_DIRECTORY -xzf $LOCKBOX_TMP
 rm -f $LOCKBOX_TMP
 
 echo "Lockbox restored!"
